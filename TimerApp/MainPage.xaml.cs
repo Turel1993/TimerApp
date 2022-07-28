cb7800b7697c3bbf9e690345f162e3630c1cfb14
Luis <Luis@LTEJEDA>
2022-07-28 12:36:50 -0500

    Agregar archivos de proyecto.

---
+++ TimerApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace TimerApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{		
+		await TimerAndroidAsync();
+    }
+
+	private async Task TimerAndroidAsync() 
+	{
+        var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+        CounterBtn.IsEnabled = false;
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+
+        while (await timer.WaitForNextTickAsync())
+        {
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            await DisplayAlert("Test", "el valor es true y se ejecuta cada 5 segundos", "Ok");
+            count++;
+            if (count <= 4)
+			{
+                timer.Dispose();
+                await DisplayAlert("Test", "Saliste del timer", "Ok");
+                CounterBtn.IsEnabled = true;
+            }
+			
+        }
+    }
+
+
+}
+
